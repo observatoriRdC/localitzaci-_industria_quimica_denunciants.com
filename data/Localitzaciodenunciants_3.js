fbc6bf8e180cefa2b36af4c845ee5a64847d331c
observatoriRdC <pousf@staperpetua.org>
2022-09-26 13:33:55 +0200

    olors tòxiques

---
+++ data/Localitzaciodenunciants_3.js
@@ -0,0 +1,16 @@
+var json_Localitzaciodenunciants_3 = {
+"type": "FeatureCollection",
+"name": "Localitzaciodenunciants_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Denunciant": "Meri Prieto", "Domicili": "C\/Sant Valentí (s\/n)", "Municipi": "Mollet del Vallès" }, "geometry": { "type": "Point", "coordinates": [ 2.207629737372881, 41.533986662755346 ] } },
+{ "type": "Feature", "properties": { "Denunciant": "Maria Barbero", "Domicili": "C\/Sant Valentí, 3", "Municipi": "Mollet del Vallès" }, "geometry": { "type": "Point", "coordinates": [ 2.208097085655708, 41.534464062124798 ] } },
+{ "type": "Feature", "properties": { "Denunciant": "Marta Ribada Corró", "Domicili": "Passeig de la Florida, 4", "Municipi": "Santa Perpètua de Mogoda" }, "geometry": { "type": "Point", "coordinates": [ 2.183982896126781, 41.531985209259986 ] } },
+{ "type": "Feature", "properties": { "Denunciant": "Sara Tejero", "Domicili": "AV DEU,    2", "Municipi": "Santa Perpètua de Mogoda" }, "geometry": { "type": "Point", "coordinates": [ 2.192522696886646, 41.519449074861853 ] } },
+{ "type": "Feature", "properties": { "Denunciant": "Eric Peregrina Monfort", "Domicili": "CTRA NACIONAL 152-Z,   10 Esc D 02 02", "Municipi": "Santa Perpètua de Mogoda" }, "geometry": { "type": "Point", "coordinates": [ 2.195315761144641, 41.518370381570612 ] } },
+{ "type": "Feature", "properties": { "Denunciant": "Carlos Vicente Ortega Huertas", "Domicili": "C DIVUIT,    5 Bl D", "Municipi": "Santa Perpètua de Mogoda" }, "geometry": { "type": "Point", "coordinates": [ 2.189199671881674, 41.520195386980177 ] } },
+{ "type": "Feature", "properties": { "Denunciant": "Pedro Iznajar Garcia", "Domicili": "C LLEIDA,    6 02 02", "Municipi": "Santa Perpètua de Mogoda" }, "geometry": { "type": "Point", "coordinates": [ 2.178564793564258, 41.531813659265538 ] } },
+{ "type": "Feature", "properties": { "Denunciant": "Cesca Mejías", "Domicili": "C\/Onze", "Municipi": "Santa Perpètua de Mogoda" }, "geometry": { "type": "Point", "coordinates": [ 2.194353071649227, 41.519142467911159 ] } },
+{ "type": "Feature", "properties": { "Denunciant": "Noelia Hermosilla Pinto", "Domicili": "PTGE CLEMENTINA ARDERIU,   18", "Municipi": "Santa Perpètua de Mogoda" }, "geometry": { "type": "Point", "coordinates": [ 2.180304156854842, 41.529790377697793 ] } }
+]
+}
